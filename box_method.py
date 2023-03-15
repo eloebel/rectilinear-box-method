5dd3ea7f5f64a0fe9855259b07f8dfd5793b7b7a
Erik Loebel <erik.loebel@tu-dresden.de>
2023-03-15 15:02:44 +0100

    Add files via upload

--- box_method.py
+++ box_method.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python -u
 #
+# Author: Erik Loebel (erik.loebel@tu-dresden.de)
+# Affiliation: TU Dresden | Geodetic Earth System Research
 # Purpose: Rectilinear box method to quantify glacier terminus position change
-# Author: Erik Loebel
-# Last change: 2022-09-05
+# Repository and documentation: https://github.com/eloebel/rectilinear-box-method
+#
+# Last change: 2023-02-15
 #
 #
 # ------------------------------------------
@@ -22,13 +25,19 @@ from tifffile import TiffFile
 from osgeo import gdal
 import cartopy.crs as ccrs
 from matplotlib import cm
+import glob
 ### ------------------------------------------
-### define glacier ###
-glacier = 'harald_moltke_brae'
-
 ### define path ###
-path = '/home_cl12/loebel/WORK/git'
+path = os.getcwd()
 
+### define glacier ###
+glacier = 'harald_moltke_brae'      # this name must correspond the file name of the box and the line-folder
+
+### define Coordinate Reference System (CRS) ###
+basemap_proj = 'UTM'   # CRS of the basemap -> UTM (UTM-Zone will be selected accordingly) EPSG code (like 4326, 3413, 3031)
+shp_proj = '3413'      # CRS of the shapefiles (box and lines) as EPSG code (like 4326, 3413, 3031); UTM currently not possible
+
+### define plot parameters ###
 linewidth=0.5                                  # linewidth of the calving front plot
 basemap=path+'/basemap/'+glacier+'.tif'        # location of the background image (should be filled in automatically using the defined glacier name)
 
@@ -74,8 +83,8 @@ cb1 = matplotlib.colorbar.ColorbarBase(ax0,cmap=cmap, norm=norm, orientation='ve
  
 ### set up projection transform ###
 wgs84 = pyproj.CRS('EPSG:4326')
-epsg_3413 = pyproj.CRS('EPSG:3413')
-project = pyproj.Transformer.from_crs(epsg_3413, wgs84, always_xy=True).transform
+epsg_shp = pyproj.CRS('EPSG:'+shp_proj+'')
+project = pyproj.Transformer.from_crs(epsg_shp, wgs84, always_xy=True).transform
 
 box_wgs84 = shapely.ops.transform(project, box_geom)        # transform box from epsg:3413 to epsg:4326
 
@@ -85,8 +94,8 @@ with TiffFile(basemap) as tif:                  # load .tif
     basemap_raster = tif.asarray()
 
 basemap = gdal.Open(basemap)
-prj=basemap.GetProjection()
-UTM_zone=(prj.split("UTM zone ",1)[1])[ 0 : 2 ] # getting UTM zone
+#prj=basemap.GetProjection()
+#UTM_zone=(prj.split("UTM zone ",1)[1])[ 0 : 2 ] # getting UTM zone
             
 width_basemap = basemap.RasterXSize             # getting width
 height_basemap = basemap.RasterYSize            # getting height
@@ -103,7 +112,14 @@ plot_extent=[box_wgs84.bounds[0], box_wgs84.bounds[2], box_wgs84.bounds[1], box_
 print('  - plotting the basemap ...')
 ax1 = fig.add_subplot(gs[0:2, 0:19],projection=ccrs.NorthPolarStereo()) 
 ax1.set_extent(plot_extent)
-ax1.imshow(basemap_raster,extent=img_extent_basemap,transform=ccrs.UTM(UTM_zone),cmap='gray') 
+if basemap_proj == 'UTM':
+    prj=basemap.GetProjection()
+    UTM_zone=(prj.split("UTM zone ",1)[1])[ 0 : 2 ]         # getting UTM zone
+    ax1.imshow(basemap_raster,extent=img_extent_basemap,transform=ccrs.UTM(UTM_zone),cmap='gray') 
+elif basemap_proj == '4326' or basemap_proj == 'WGS84':
+    ax1.imshow(basemap_raster,extent=img_extent_basemap,transform=ccrs.PlateCarree(),cmap='gray')
+else:
+    ax1.imshow(basemap_raster,extent=img_extent_basemap,transform=ccrs.epsg(basemap_proj),cmap='gray')
 gl = ax1.gridlines(draw_labels=True, x_inline=False, y_inline=False,color='k',alpha=0.3, linestyle='--')
 gl.top_labels = True
 gl.right_labels = True
@@ -112,14 +128,18 @@ gl.left_labels = True
 
 ### plotting box in transparent white ###
 xs, ys = box_geom.exterior.xy
-ax1.fill(xs, ys,transform=ccrs.epsg(3413), alpha=0.3, fc='white')
-
+if shp_proj == '4326' or shp_proj == 'WGS84':
+    ax1.fill(xs, ys,transform=ccrs.PlateCarree(), alpha=0.3, fc='white')
+else:
+    ax1.fill(xs, ys,transform=ccrs.epsg(shp_proj), alpha=0.3, fc='white')
 ### looping through all lines (.shp files) and (1.) calculating area change and (2.) plotting them on the map (with color according to decimal year date) ###
 print('  - calculating area change ...')
 area = np.zeros(len(lines))
 for i, line in tqdm(enumerate(lines), total=len(lines)):
     ### load line ###
-    line = shapefile.Reader(path+'/lines/'+glacier+'/'+line+'/calving_front_'+line+'_3413.shp')
+    shp_path = path+'/lines/'+glacier+'/'+line+'/*.shp'
+    linepath = glob.glob(shp_path)[0]
+    line = shapefile.Reader(str(linepath))
     feature = line.shapeRecords()[0]
     line = feature.shape.__geo_interface__  
     line_geom = shapely.geometry.shape(line)
@@ -146,12 +166,16 @@ for i, line in tqdm(enumerate(lines), total=len(lines)):
     area_box_1=geom_area.area
     area[i]=area_box_1
     ### plotting line on map ###
-    ax1.plot(*out.geoms[0].exterior.xy,transform=ccrs.epsg(3413),linewidth=linewidth, color=cmap(norm(float(dates_dec[i]))))
+    if shp_proj == '4326' or shp_proj == 'WGS84':
+        ax1.plot(*out.geoms[0].exterior.xy,transform=ccrs.PlateCarree(),linewidth=linewidth, color=cmap(norm(float(dates_dec[i]))))
+    else:
+        ax1.plot(*out.geoms[0].exterior.xy,transform=ccrs.epsg(shp_proj),linewidth=linewidth, color=cmap(norm(float(dates_dec[i]))))
+    
 
 ### plot box exterior in black ###
-ax1.plot(*box_geom.exterior.xy,transform=ccrs.epsg(3413),linewidth=linewidth+0.5,color="black")
+ax1.plot(*box_wgs84.exterior.xy,transform=ccrs.PlateCarree(),linewidth=linewidth+0.5,color="black")
 
-print('  - plotting time series ...')
+print('  - saving OVERVIEW_'+glacier+'.png ...')
 ### final area in km2 -> assumption that glacier is retrating ###
 if area[0] >= area[len(area)-1]:
     area_final=(area-area[0])/1000000
@@ -178,3 +202,8 @@ fig.subplots_adjust(hspace=0.2)
 plt.savefig(path+'/OVERVIEW_' + glacier +'.png')
 plt.close()
 
+print('  - saving results to terminus_change_'+glacier+'.txt ...')
+### saving txt with area change ###
+np.savetxt(path+'/terminus_change_'+glacier+'.txt', np.c_[dates_dec, area_final],
+               header='date (year) area (km^2)', fmt='%10.3f',
+               delimiter='\t')
